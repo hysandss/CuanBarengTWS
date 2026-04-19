b9c651df76c5b4e976d85193865c2c7440bb155a
hysandss <ressakun715@gmail.com>
2026-04-19 21:04:42 +0700

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,458 @@
+//list video
+
+var VIDEO_LIST = [
+  {
+    folder: "On Boarding",
+    link:  "https://youtu.be/pZurF74GPzY?si=6yxEnDtGzh9RC9cA",
+    title: "1. Navigating Through Discord",
+    tags:  [ "Trade With Suli", "BAB 0", "On Boarding"]
+  },
+  {
+    folder: "On Boarding",
+    link:  "https://youtu.be/S_y29zNXfpI?si=6Qf43jsmlTiz40M4",
+    title: "2. Risk Management",
+    tags:  [ "Trade With Suli", "BAB 0", "On Boarding"]
+  },
+  {
+    folder: "On Boarding",
+    link:  "https://youtu.be/rcPwV6oHrxk?si=HM8Z01Ah6cE3ufyK",
+    title: "3. Sunk Cost Fallacy",
+    tags:  [ "Trade With Suli", "BAB 0", "On Boarding"]
+  },
+  {
+    folder: "On Boarding",
+    link:  "https://youtu.be/iRZsws6gqkY?si=Ll02jgpsdqBVXXzV",
+    title: "4. Hope Is Not an Investment Strategy",
+    tags:  [ "Trade With Suli", "BAB 0", "On Boarding"]
+  },
+  {
+    folder: "Fundamental Research",
+    link:  "https://youtu.be/pdwGWCTSoK8?si=RZtEG0Tw5d14y1cn",
+    title: "1. Fundamental Crypto Coins",
+    tags:  [ "Trade With Suli", "BAB 0", "Fundamental Research"]
+  },
+  {
+    folder: "Fundamental Research",
+    link:  "https://youtu.be/6O2ZBSv8XsQ?si=d6j-YnnKdbywOj4R",
+    title: "2. Crypto Screening",
+    tags:  [ "Trade With Suli", "BAB 0", "Fundamental Research"]
+  },
+  {
+    folder: "Cara Memilih Crypto Exchange",
+    link:  "https://youtu.be/OCoG6cTolYI?si=m1wf5w0sjO1KujLQ",
+    title: "1. Pakai Exchange Apa?",
+    tags:  [ "Trade With Suli", "BAB 0", "Crypto Exchange"]
+  },
+  {
+    folder: "Cara Memilih Crypto Exchange",
+    link:  "https://youtu.be/K3lhFA0Yukg?si=RiwpGDbIN8oZYi-E",
+    title: "2. Cari Koin Crypto",
+    tags:  [ "Trade With Suli", "BAB 0", "Crypto Exchange"]
+  },
+  {
+    folder: "Cara Memilih Crypto Exchange",
+    link:  "https://youtu.be/GsObJgqV2Tk?si=92lBvRXIA71t9vLa",
+    title: "3. Beli Posisi Spot di Cex",
+    tags:  [ "Trade With Suli", "BAB 0", "Crypto Exchange"]
+  },
+  {
+    folder: "Leverage, Margin & Order Type",
+    link:  "https://youtu.be/tJYiS0Sv-aY?si=3Mprbx9lYrj3hNjF",
+    title: "1. Apa Itu Leverage?",
+    tags:  [ "Trade With Suli", "BAB 0", "CRYPTO TRADING"]
+  },
+  {
+    folder: "Leverage, Margin & Order Type",
+    link:  "https://youtu.be/lI7AQ3yaapE?si=FcRPG24b5tnV3OvZ",
+    title: "2. Apa Itu Margin Type?",
+    tags:  [ "Trade With Suli", "BAB 0", "CRYPTO TRADING"]
+  },
+  {
+    folder: "Leverage, Margin & Order Type",
+    link:  "https://youtu.be/N7YrMgv83es?si=UOIr2bdzdF6tLGvq",
+    title: "3. Order Type",
+    tags:  [ "Trade With Suli", "BAB 0", "CRYPTO TRADING"]
+  },
+  {
+    folder: "Cara Pakai TradingView",
+    link:  "https://youtu.be/Ct7kL8M8UVk?si=DhVByXrFK-Oktfo9",
+    title: "1. Charting Software I",
+    tags:  [ "Trade With Suli", "BAB 0", "CRYPTO TRADING"]
+  },
+  {
+    folder: "Cara Pakai TradingView",
+    link:  "https://youtu.be/liynRY-DkRY?si=NTIGK3FOHFW_qUaR",
+    title: "2. Charting Software II",
+    tags:  [ "Trade With Suli", "BAB 0", "CRYPTO TRADING"]
+  },
+  {
+    folder: "Cara Pakai TradingView",
+    link:  "https://youtu.be/2nPHySd2N9Y?si=tDOeXwHYL9Gkd45Z",
+    title: "3. Charting Software III",
+    tags:  [ "Trade With Suli", "BAB 0", "CRYPTO TRADING"]
+  },
+  {
+    folder: "On Boarding",
+    link:  "https://youtu.be/EN9YiGq6K2A?si=tqMaGkcyAJFROojD",
+    title: "Apakah Crypto Judi atau Investasi?",
+    tags:  [ "Trade With Suli", "BAB 0", "On Boarding"]
+  },
+  {
+    folder: "On Boarding",
+    link:  "https://youtu.be/EzWnpHn4tj0?si=cbTnkNjkWhzA1ppr",
+    title: "Diversifikasi",
+    tags:  [ "Trade With Suli", "BAB 0", "On Boarding"]
+  },
+  {
+    folder: "Mind & Money Management",
+    link:  "https://youtu.be/RcmdwzW5rqA?si=hV4KPWVcNLByPDOm",
+    title: "FAQ & Mind Management",
+    tags:  [ "Trade With Suli", "BAB 0", "Mind Management"]
+  },
+
+  
+  {
+    folder: "Technical Research",
+    link:  "https://youtu.be/Cj0ODAlhoy0?si=laa4Gka4sIrNu7pE",
+    title: "1. Liquidity Sweep",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://youtu.be/2M_A9FG_oHQ?si=yIa6nVw5R9veOiPB",
+    title: "2. Market Gap",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://youtu.be/bRcab8Ytl9k?si=6O-V_-341V1BAxNz",
+    title: "3. Candle Stick Pattern",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://youtu.be/5E1UQi4MwLs?si=a_6hsDA2krN6_9Lm",
+    title: "4. Trendline Breakout",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://youtu.be/Bp6J-Yy9kxs?si=LoGUxOv7BsLVGUPl",
+    title: "5. Advance Fibbonacci",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://youtu.be/cTyb9pb2cDY?si=Q-MAit9Wc7hZlY4k",
+    title: "6. Chart Pattern",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://youtu.be/6vHXOJHwHCE?si=MdhgGr65LQ3gE1x6",
+    title: "7. Market Volume",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://youtu.be/VgjjSHkmVzA?si=pLRzsZt1NcvVw1fh",
+    title: "8. Order Block",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+
+
+  {
+    folder: "Trading Preparation",
+    link:  "https://youtu.be/Z5BHrkJgwOU?si=oEbVccdxBiuPzs7H",
+    title: "1. RSI",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Trading Preparation",
+    link:  "https://youtu.be/XuoYpnWkwow?si=vl9UFZyoJjDIZbtp",
+    title: "2. What to Buy, What to Avoid",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Trading Preparation",
+    link:  "https://youtu.be/akWDoJxjSc0?si=AHrPh0fsyJsE6MQE",
+    title: "3. Point Of Interest",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+
+  
+  {
+    folder: "Rahasia Trading Menggunakan Indikator",
+    link:  "https://youtu.be/wq6IGuc1pP4?si=-AqGSqIb8QRJJoHP",
+    title: "1. Crypto Indicator Basic",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Indicator"]
+  },
+  {
+    folder: "Rahasia Trading Menggunakan Indikator",
+    link:  "https://youtu.be/HMjbpaUqfPU?si=aOVBGU8JqTMlPPos",
+    title: "2. Cara Menggunakan Indicator",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Indicator"]
+  },
+  {
+    folder: "Rahasia Trading Menggunakan Indikator",
+    link:  "https://youtu.be/tt4DBSf2frk?si=CgJ-oyEjnFwJQ6dG",
+    title: "3. Rahasia Menggunakan Indicator",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Indicator"]
+  },
+
+
+  {
+    folder: "DEX Trading",
+    link:  "https://youtu.be/b_wtS4UbcQc?si=c9lG5fAwA-4cn7Rd",
+    title: "1. DEX Tutorial dari 0",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "DEX"]
+  },
+  {
+    folder: "DEX Trading",
+    link:  "https://youtu.be/8Oea1l9l9NE?si=y_3IVCQf4jn9WAXp",
+    title: "2. Beli Spot Di DEX",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "DEX"]
+  },
+  {
+    folder: "DEX Trading",
+    link:  "https://youtu.be/QOf9tm-bx5c?si=GoEMc9I2gzLS3rpV",
+    title: "3. Bridging Antara Blockchain",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "DEX"]
+  },
+  {
+    folder: "DEX Trading",
+    link:  "https://youtu.be/zdWmXYItKHM?si=ZSj8eUMfl9-UoJoK",
+    title: "4. Metamask Guide",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "DEX"]
+  },
+
+  
+  {
+    folder: "Macroeconomics Series",
+    link:  "https://www.youtube.com/watch?v=x_GFvuXVP98&list=PL1hlgGm52pkTRBXZ0JP02BBzpFIEjnWsY&index=1&pp=iAQB",
+    title: "1. Business Cycle",
+    tags:  [ "Trade With Suli", "BAB 1", "Macroeconomics"]
+  },
+  {
+    folder: "Macroeconomics Series",
+    link:  "https://www.youtube.com/watch?v=cnUA0zPwXnA&list=PL1hlgGm52pkTRBXZ0JP02BBzpFIEjnWsY&index=2&pp=iAQB0gcJCdoKAYcqIYzv",
+    title: "2. Central Bank VS Private Bank",
+    tags:  [ "Trade With Suli", "BAB 1", "Macroeconomics"]
+  },
+  {
+    folder: "Macroeconomics Series",
+    link:  "https://www.youtube.com/watch?v=_Oaht0YZ87U&list=PL1hlgGm52pkTRBXZ0JP02BBzpFIEjnWsY&index=3&pp=iAQB",
+    title: "3. Quantitative Easing VS Quantitative Tightening",
+    tags:  [ "Trade With Suli", "BAB 1", "Macroeconomics"]
+  },
+  {
+    folder: "Macroeconomics Series",
+    link:  "https://www.youtube.com/watch?v=VslUU0IzA_s&list=PL1hlgGm52pkTRBXZ0JP02BBzpFIEjnWsY&index=4&pp=iAQB",
+    title: "4. Fed Liquidity",
+    tags:  [ "Trade With Suli", "BAB 1", "Macroeconomics"]
+  },
+  {
+    folder: "Macroeconomics Series",
+    link:  "https://www.youtube.com/watch?v=YiFKEECRoJc&list=PL1hlgGm52pkTRBXZ0JP02BBzpFIEjnWsY&index=5&pp=iAQB",
+    title: "5. Bond Market & Volatility",
+    tags:  [ "Trade With Suli", "BAB 1", "Macroeconomics"]
+  },
+
+  
+  {
+    folder: "Technical Research",
+    link:  "https://www.youtube.com/watch?v=s_1Zv6jrZ7s&list=PL1hlgGm52pkR9YZvttcFMFDF7Ti9drQaX&index=1&pp=iAQB",
+    title: "1. Mean Reverting VS Trending Market",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://www.youtube.com/watch?v=f8W5bZLPGp8&list=PL1hlgGm52pkR9YZvttcFMFDF7Ti9drQaX&index=2&pp=iAQB",
+    title: "2. Apa Itu Trending Market",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://www.youtube.com/watch?v=lEeTZD3pc_M&list=PL1hlgGm52pkR9YZvttcFMFDF7Ti9drQaX&index=3&pp=iAQB",
+    title: "3. Kenapa Indicator Trend Following Underperfom",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://www.youtube.com/watch?v=Ik3-5GVWwEs&list=PL1hlgGm52pkR9YZvttcFMFDF7Ti9drQaX&index=4&pp=iAQB",
+    title: "4. Cara Determinasi Market Environment",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://www.youtube.com/watch?v=HaQn11rhejU&list=PL1hlgGm52pkR9YZvttcFMFDF7Ti9drQaX&index=5&pp=iAQB",
+    title: "5. Cara Melakukan Trend Following Medium Term",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+  {
+    folder: "Technical Research",
+    link:  "https://www.youtube.com/watch?v=Mv6uEAcbiPg&list=PL1hlgGm52pkR9YZvttcFMFDF7Ti9drQaX&index=6&pp=iAQB",
+    title: "6. Cara Melakukan Trend Following Long Term",
+    tags:  [ "Trade With Suli", "BAB 1", "CRYPTO TRADING", "Technical Research"]
+  },
+
+
+  {
+    folder: "Auction Market Theory",
+    link:  "https://www.youtube.com/watch?v=EVXeiPGWFXM&list=PL1hlgGm52pkSwNAyweiSAyTBsnOW2MV6k&index=1&pp=iAQB",
+    title: "1. Intro Auction Market Theory",
+    tags:  [ "Trade With Suli", "BAB 2", "CRYPTO TRADING", "Auction Market Theory"]
+  },
+  {
+    folder: "Auction Market Theory",
+    link:  "https://www.youtube.com/watch?v=zxrgjD13YG8&list=PL1hlgGm52pkSwNAyweiSAyTBsnOW2MV6k&index=2&pp=iAQB",
+    title: "2. Balance Market VS Imbalance Market",
+    tags:  [ "Trade With Suli", "BAB 2", "CRYPTO TRADING", "Auction Market Theory"]
+  },
+  {
+    folder: "Auction Market Theory",
+    link:  "https://www.youtube.com/watch?v=hUPaBojdD_Q&list=PL1hlgGm52pkSwNAyweiSAyTBsnOW2MV6k&index=4&pp=iAQB",
+    title: "3. Cara Menggunakan Market Profile TPO",
+    tags:  [ "Trade With Suli", "BAB 2", "CRYPTO TRADING", "Auction Market Theory"]
+  },
+  {
+    folder: "Auction Market Theory",
+    link:  "https://www.youtube.com/watch?v=Rua2jDNCjpk&list=PL1hlgGm52pkSwNAyweiSAyTBsnOW2MV6k&index=5&pp=iAQB",
+    title: "4. Cara Menggunakan Volume Profile",
+    tags:  [ "Trade With Suli", "BAB 2", "CRYPTO TRADING", "Auction Market Theory"]
+  },
+  {
+    folder: "Auction Market Theory",
+    link:  "https://www.youtube.com/watch?v=lKki_1dyPi0&list=PL1hlgGm52pkSwNAyweiSAyTBsnOW2MV6k&index=3&pp=iAQB",
+    title: "5. VWAP Explained",
+    tags:  [ "Trade With Suli", "BAB 2", "CRYPTO TRADING", "Auction Market Theory"]
+  },
+  {
+    folder: "Auction Market Theory",
+    link:  "https://www.youtube.com/watch?v=4BxatkUefJE&list=PL1hlgGm52pkSwNAyweiSAyTBsnOW2MV6k&index=6&pp=iAQB",
+    title: "6. How To Define Range",
+    tags:  [ "Trade With Suli", "BAB 2", "CRYPTO TRADING", "Auction Market Theory"]
+  },
+
+
+  {
+    folder: "Trade Setup",
+    link:  "https://www.youtube.com/watch?v=AwCW7D_haLY&list=PL1hlgGm52pkSxKxW7GZmrSdfxXUP2oVoY&index=1&pp=iAQB",
+    title: "1. How To Trade In Ranging Market",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Trade Setup"]
+  },
+  {
+    folder: "Trade Setup",
+    link:  "https://www.youtube.com/watch?v=BL2PHRZ7drg&list=PL1hlgGm52pkSxKxW7GZmrSdfxXUP2oVoY&index=2&pp=iAQB",
+    title: "2. How To Trade In Trending Market",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Trade Setup"]
+  },
+  {
+    folder: "Trade Setup",
+    link:  "https://www.youtube.com/watch?v=vknMe-BuIEs&list=PL1hlgGm52pkSxKxW7GZmrSdfxXUP2oVoY&index=3&pp=iAQB",
+    title: "3. How To Find Your Edge",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Trade Setup"]
+  },
+
+  
+  {
+    folder: "Platform For Analyze",
+    link:  "https://www.youtube.com/watch?v=6WjTqiPIwn0&list=PL1hlgGm52pkSzJSsjaRJZxcH0SyNITeww&index=1&pp=iAQB",
+    title: "1. Platform Setup Advance TradingView",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Platform For Analyze"]
+  },
+  {
+    folder: "Platform For Analyze",
+    link:  "https://www.youtube.com/watch?v=tyK97foeG_Y&list=PL1hlgGm52pkSzJSsjaRJZxcH0SyNITeww&index=2&pp=iAQB",
+    title: "2. Platform Setup Advance Coinank",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Platform For Analyze"]
+  },
+
+  
+  {
+    folder: "Time Based Key Level",
+    link:  "https://www.youtube.com/watch?v=f1gcFpp8CHM&list=PL1hlgGm52pkSuyM4Ev0cB3LkijFh3R0Tj&index=1&pp=iAQB",
+    title: "1. Time Based Key Levels",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Time Based Key Level"]
+  },
+  {
+    folder: "Time Based Key Level",
+    link:  "https://www.youtube.com/watch?v=P7JCNEVMeUI&list=PL1hlgGm52pkSuyM4Ev0cB3LkijFh3R0Tj&index=2&pp=iAQB",
+    title: "2. Monday Range",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Time Based Key Level"]
+  },
+  {
+    folder: "Time Based Key Level",
+    link:  "https://www.youtube.com/watch?v=AdBHMkOTQ2U&list=PL1hlgGm52pkSuyM4Ev0cB3LkijFh3R0Tj&index=3&pp=iAQB",
+    title: "3. Trading Session",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Time Based Key Level"]
+  },
+
+  
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=R_ZQ-qUqDqM&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=1&pp=iAQB",
+    title: "1. Footprint Chart",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=DvxVAmqkUMk&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=2&pp=iAQB",
+    title: "2. Depth of Market",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=E1HtL6IPZDI&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=3&pp=iAQB0gcJCdoKAYcqIYzv",
+    title: "3. What is CVD",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=T7Fzn1YX_YI&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=4&pp=iAQB",
+    title: "4. What is OI",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=rgvYXhUHjss&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=5&pp=iAQB",
+    title: "5. What is Liquidation",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=DjosG8gL3og&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=6&pp=iAQB",
+    title: "6. What is Funding Rates",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=XJluqaOYiV4&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=7&pp=iAQB",
+    title: "7. How to Spot Absorption Using Footprint",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=SRRSbnqQlOc&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=8&pp=iAQB",
+    title: "8. Positions Closing VS Positions Entering",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=v9zXPPmq8E8&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=9&pp=iAQB",
+    title: "9. Order Block Explained",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=jrD31jJ1V-M&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=10&pp=iAQB",
+    title: "10. Orderbook Depth",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+  {
+    folder: "How to Read Orderflow",
+    link:  "https://www.youtube.com/watch?v=2SWvq1W2n90&list=PL1hlgGm52pkQc6L89TjjkfUgg0BSzPmZS&index=11&pp=iAQB",
+    title: "11. How to Use Relative Volume",
+    tags:  [ "Trade With Suli", "BAB 3", "CRYPTO TRADING", "Order Flow"]
+  },
+];
